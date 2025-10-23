0751af0ba5e943918dea2d7a431a5fc407fb0075
HiteshPrajapati <hiteshprajapati185@gmail.com>
2025-10-23 19:29:07 +0530

    fix build issue for prisma orm

--- server/dist/helpers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = exports.TryCatch = exports.resHandler = void 0;
-const resHandler_1 = require("./resHandler");
-Object.defineProperty(exports, "resHandler", { enumerable: true, get: function () { return resHandler_1.resHandler; } });
-const TryCatch_1 = require("./TryCatch");
-Object.defineProperty(exports, "TryCatch", { enumerable: true, get: function () { return TryCatch_1.TryCatch; } });
-const asyncHandler_1 = require("./asyncHandler");
-Object.defineProperty(exports, "asyncHandler", { enumerable: true, get: function () { return asyncHandler_1.asyncHandler; } });
