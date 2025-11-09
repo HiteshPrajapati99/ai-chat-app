3fc1fc7f6b8cc065b4a77acedc3e9c33718e5e91
HiteshPrajapati <hiteshprajapati185@gmail.com>
2025-11-09 23:44:32 +0530

    fix env issue

--- server/dist/modules/user/schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userSchema = void 0;
-const zod_1 = require("zod");
-exports.userSchema = zod_1.z.object({
-    name: zod_1.z.string().optional(),
-});
